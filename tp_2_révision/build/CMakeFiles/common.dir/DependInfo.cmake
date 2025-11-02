67f61008c9511db6fcd216762d7b374c69120f53
saamal <saamal@vm-saamal.local.isima.fr>
2025-11-02 15:31:26 +0100

    implementation de la class Nuage, generique sur le type de points à contenir

---
+++ tp_2_révision/build/CMakeFiles/common.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/saamal/shared/ZZ3/CPP/Révision/tp_2_révision/src/cartesien.cpp" "CMakeFiles/common.dir/src/cartesien.cpp.o" "gcc" "CMakeFiles/common.dir/src/cartesien.cpp.o.d"
+  "/home/saamal/shared/ZZ3/CPP/Révision/tp_2_révision/src/point.cpp" "CMakeFiles/common.dir/src/point.cpp.o" "gcc" "CMakeFiles/common.dir/src/point.cpp.o.d"
+  "/home/saamal/shared/ZZ3/CPP/Révision/tp_2_révision/src/polaire.cpp" "CMakeFiles/common.dir/src/polaire.cpp.o" "gcc" "CMakeFiles/common.dir/src/polaire.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tp_2_révision/src/nuage.hpp
@@ -0,0 +1,67 @@
+#ifndef NUAGE_HPP_780720829
+#define NUAGE_HPP_780720829
+
+#include <iostream>
+#include <vector>
+
+#include "cartesien.hpp"
+#include "polaire.hpp"
+#include "point.hpp"
+
+
+template <typename T>
+class Nuage {
+    private:
+        std::vector<T> points;
+
+    public:
+        Nuage() = default;
+
+        using iterator = typename std::vector<T>::iterator;
+        using const_iterator = typename std::vector<T>::const_iterator;
+
+        const_iterator begin() const;
+        const_iterator end() const;
+
+        iterator begin();
+        iterator end();  
+
+        size_t size() const;
+        
+        void ajouter(const T & t); 
+};
+
+// template <typename T>
+// Nuage<T>::Nuage() {}
+
+template <typename T>
+typename Nuage<T>::const_iterator Nuage<T>::begin() const {
+    return points.begin();
+}
+
+template <typename T>
+typename Nuage<T>::const_iterator Nuage<T>::end() const {
+    return points.end();
+}
+
+template <typename T>
+typename Nuage<T>::iterator Nuage<T>::begin() {
+    return points.begin();
+}
+
+template <typename T>
+typename Nuage<T>::iterator Nuage<T>::end() {
+    return points.end();
+}
+
+template <typename T>
+size_t Nuage<T>::size() const {
+    return points.size();
+}
+
+template <typename T>
+void Nuage<T>::ajouter(const T & t) {
+    points.push_back(t);
+}
+
+#endif
